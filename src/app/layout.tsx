483ce6eb5cfed4e93daf111b1a2f750ab2c32bef
lumpi2k <32732790+lumpi2k@users.noreply.github.com>
2024-02-01 21:25:15 +0100

    auto dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Theme, ThemePanel, Container, Section } from "@radix-ui/themes";
 
 import Navbar from "@/app/components/ui/navbar.js";
 import Footer from "@/app/components/ui/footer.js";
+import { Providers } from "@/app/components/ui/providers.jsx";
 
 export const metadata: Metadata = {
   title: "Mondu Demo App",
@@ -19,27 +20,28 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <Theme
-          appearance="dark"
-          accentColor="purple"
-          grayColor="slate"
-          panelBackground="solid"
-          scaling="110%"
-          radius="small"
-        >
-          {/* <ThemePanel /> */}
-          <Container size="4">
-            <Section pt="0" pb="4">
-              <Navbar />
-            </Section>
-            <Section pt="4" pb="4">
-              {children}
-            </Section>
-            <Section pt="4" pb="0">
-              <Footer />
-            </Section>
-          </Container>
-        </Theme>
+        <Providers>
+          <Theme
+            accentColor="purple"
+            grayColor="slate"
+            panelBackground="solid"
+            scaling="110%"
+            radius="small"
+          >
+            {/* <ThemePanel /> */}
+            <Container size="4">
+              <Section pt="0" pb="4">
+                <Navbar />
+              </Section>
+              <Section pt="4" pb="4">
+                {children}
+              </Section>
+              <Section pt="4" pb="0">
+                <Footer />
+              </Section>
+            </Container>
+          </Theme>
+        </Providers>
       </body>
     </html>
   );

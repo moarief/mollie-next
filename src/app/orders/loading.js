d974602f3aefaca1349d94647c55f91e4ecc8a97
lumpi2k <32732790+lumpi2k@users.noreply.github.com>
2024-02-04 21:01:14 +0100

    better skeleton

--- src/app/orders/loading.js
+++ src/app/orders/loading.js
@@ -1,9 +1,14 @@
-import { Flex, Card } from "@radix-ui/themes";
+import { Flex, Card, Heading } from "@radix-ui/themes";
 
 export default function Loading() {
   return (
-    <Flex align="center" justify="center">
-      <Card>Loading...</Card>
-    </Flex>
+    <main>
+      <Flex direction="column" m="6">
+        <Heading>Orders</Heading>
+        <Flex align="center" justify="center" pt="4">
+          <Card>Loading...</Card>
+        </Flex>
+      </Flex>
+    </main>
   );
 }

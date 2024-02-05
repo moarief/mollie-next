8ab6df6396e42fd767fb693ed023391d45021e90
hreinberger <40197225+hreinberger@users.noreply.github.com>
2024-02-05 17:49:39 +0100

    add Postgres and Prisma ORM for persistence

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,6 @@
 import { headers } from "next/headers";
 import MonduVerifier from "@/app/lib/webhookVerifier";
+import { PrismaClient } from "@prisma/client";
 
 export async function POST(request: Request) {
   try {
@@ -11,6 +12,31 @@ export async function POST(request: Request) {
     if (!isVerified) {
       throw new Error("Webhook verification failed");
     }
+    const webhookData = JSON.parse(payload);
+    // destructure webhookData object into variables
+    const {
+      topic,
+      order_uuid,
+      order_state,
+      event_time,
+    }: {
+      topic: string;
+      order_uuid: string;
+      order_state: string;
+      event_time: Date;
+    } = webhookData;
+
+    const primsa = new PrismaClient();
+
+    const webhook = await primsa.webhooks.create({
+      data: {
+        topic,
+        order_uuid,
+        order_state,
+        event_time,
+        payload,
+      },
+    });
 
     // Process the webhook payload
     return new Response("Webhook successfully verified", {

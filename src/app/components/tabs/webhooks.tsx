3ae1552b51f0195e2cfa75ff687540d500aefe64
hreinberger <40197225+hreinberger@users.noreply.github.com>
2024-02-06 13:43:57 +0100

    show webhook payload in modal

--- src/app/components/tabs/webhooks.tsx
+++ src/app/components/tabs/webhooks.tsx
@@ -7,9 +7,15 @@ import {
   TableRowHeaderCell,
   TableRow,
   TableCell,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+  Button,
+  DialogContent,
+  ScrollArea,
+  Separator,
 } from "@radix-ui/themes";
 import { Key } from "react";
-import StateBadge from "../ui/orderStateBadge";
 import { prisma } from "@/app/lib/db";
 
 export default async function WebhooksTab(order: { uuid: string }) {
@@ -33,10 +39,27 @@ export default async function WebhooksTab(order: { uuid: string }) {
               </Text>
             </TableRowHeaderCell>
             <TableCell>
-              <StateBadge state={entry.order_state} />
+              <Text>{entry.topic}</Text>
             </TableCell>
             <TableCell>
-              <Text>{entry.topic}</Text>
+              <DialogRoot>
+                <DialogTrigger>
+                  <Button size="1" variant="outline">
+                    Show Payload
+                  </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogTitle>Webhook Payload</DialogTitle>
+                  <Separator my="2" size="4" />
+                  <ScrollArea>
+                    <Text size="1">
+                      <pre>
+                        {JSON.stringify(JSON.parse(entry.payload), null, 2)}
+                      </pre>
+                    </Text>
+                  </ScrollArea>
+                </DialogContent>
+              </DialogRoot>
             </TableCell>
           </TableRow>
         ))}

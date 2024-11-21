6c753d3a459a121936e1fe6a3c36b6177ee93727
Hannes Reinberger <40197225+hreinberger@users.noreply.github.com>
2024-11-21 14:32:07 +0100

    optimize images

--- src/app/components/form/paymentlogo.tsx
+++ src/app/components/form/paymentlogo.tsx
@@ -1,11 +1,14 @@
 import { Flex } from '@radix-ui/themes';
+import Image from 'next/image';
 
 export default async function PaymentLogo({ method }: { method: string }) {
     return (
         <Flex>
-            <img
+            <Image
                 src={`https://mollie.com/external/icons/payment-methods/${method}.svg`}
                 alt={method}
+                height={32}
+                width={32}
             />
         </Flex>
     );

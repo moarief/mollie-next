b3181e08d48e7718e393918ca95df4be76427bb3
Hannes Reinberger <40197225+hreinberger@users.noreply.github.com>
2024-11-21 13:17:24 +0100

    fix linting errors

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -1,39 +1,47 @@
-"use client"
+'use client';
 
-import { RocketIcon } from "@radix-ui/react-icons";
-import { Callout, Flex, Heading } from "@radix-ui/themes";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { RocketIcon } from '@radix-ui/react-icons';
+import { Callout, Flex, Heading } from '@radix-ui/themes';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
+    const router = useRouter();
 
-  const router = useRouter();
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            router.push('/checkout');
+        }, 7000);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      router.push("/checkout");
-    }, 7000);
+        return () => clearTimeout(timer);
+    }, [router]);
 
-    return () => clearTimeout(timer);
-  }, []);
-
-return (
-  <main>
-    <Flex direction="column" m="6">
-      <Heading>Success</Heading>
-      <Flex align="center" justify="center" mt="4">
-        <Callout.Root size="3" color="green">
-          <Callout.Icon>
-            <RocketIcon />
-          </Callout.Icon>
-          <Callout.Text>Your Order is authorized! You'll be redirected to the checkout soon.</Callout.Text>
-        </Callout.Root>
-      </Flex>
-    </Flex>
-  </main>
-);
+    return (
+        <main>
+            <Flex
+                direction="column"
+                m="6"
+            >
+                <Heading>Success</Heading>
+                <Flex
+                    align="center"
+                    justify="center"
+                    mt="4"
+                >
+                    <Callout.Root
+                        size="3"
+                        color="green"
+                    >
+                        <Callout.Icon>
+                            <RocketIcon />
+                        </Callout.Icon>
+                        <Callout.Text>
+                            Your Order is authorized! You'll be redirected to
+                            the checkout soon.
+                        </Callout.Text>
+                    </Callout.Root>
+                </Flex>
+            </Flex>
+        </main>
+    );
 }
-
-
-
-

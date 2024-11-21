de67ec7236a48c5407f0e0c7a34407121cfe41b2
Hannes Reinberger <40197225+hreinberger@users.noreply.github.com>
2024-11-21 10:54:51 +0100

    auto redirect from the success page

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -1,20 +1,39 @@
+"use client"
+
 import { RocketIcon } from "@radix-ui/react-icons";
 import { Callout, Flex, Heading } from "@radix-ui/themes";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
-  return (
-    <main>
-      <Flex direction="column" m="6">
-        <Heading>Success</Heading>
-        <Flex align="center" justify="center" mt="4">
-          <Callout.Root size="3" color="green">
-            <Callout.Icon>
-              <RocketIcon />
-            </Callout.Icon>
-            <Callout.Text>Your Order is authorized!</Callout.Text>
-          </Callout.Root>
-        </Flex>
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/checkout");
+    }, 7000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+return (
+  <main>
+    <Flex direction="column" m="6">
+      <Heading>Success</Heading>
+      <Flex align="center" justify="center" mt="4">
+        <Callout.Root size="3" color="green">
+          <Callout.Icon>
+            <RocketIcon />
+          </Callout.Icon>
+          <Callout.Text>Your Order is authorized! You'll be redirected to the checkout soon.</Callout.Text>
+        </Callout.Root>
       </Flex>
-    </main>
-  );
+    </Flex>
+  </main>
+);
 }
+
+
+
+

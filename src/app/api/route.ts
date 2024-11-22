ece5d31e19c0e5a8e10f31af9a4d57233123eaa7
Hannes Reinberger <40197225+hreinberger@users.noreply.github.com>
2024-11-22 17:20:38 +0100

    revalidate payments page whenever a webhook comes in

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,16 +1,17 @@
-import { headers } from "next/headers";
+import { headers } from 'next/headers';
+import { revalidatePath } from 'next/cache';
 
 export async function POST(request: Request) {
-  try {
-    const headersList = await headers();
-    const payload = await request.text();
-    console.log("Received webhook payload:", payload);
-    return new Response("Webhook successfully received", {
-      status: 200,
-    });
-  } catch (error: any) {
-    return new Response(`Webhook error: ${error.message}`, {
-      status: 403,
-    });
-  }
+    try {
+        const payload = await request.text();
+        console.log('Received webhook payload:', payload);
+        revalidatePath('/payments');
+        return new Response('Webhook successfully received', {
+            status: 200,
+        });
+    } catch (error: any) {
+        return new Response(`Webhook error: ${error.message}`, {
+            status: 403,
+        });
+    }
 }

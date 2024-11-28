825de559c2d584c604b1c677395e2bf1745381bb
Hannes Reinberger <40197225+hreinberger@users.noreply.github.com>
2024-11-28 15:46:21 +0100

    comments

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,8 +2,10 @@ import { revalidatePath } from 'next/cache';
 
 export async function POST(request: Request) {
     try {
+        // Parse the incoming webhook payload and log it
         const payload = await request.text();
         console.log('Received webhook payload:', payload);
+        // Every time a webhook comes in, we revalidate the /payments page
         revalidatePath('/payments');
         return new Response('Webhook successfully received', {
             status: 200,

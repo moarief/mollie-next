9e3a5d77fb62cdc1042a50c8c2332050990e78c2
Hannes Reinberger <40197225+hreinberger@users.noreply.github.com>
2024-12-17 09:39:43 +0100

    invalidate checkout cache from time to time to allow for new payment methods to show up

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,9 +1,12 @@
-import CheckoutForm from "../components/form/checkoutform";
+import CheckoutForm from '../components/form/checkoutform';
+
+// invalidate page cache every 5 minutes to pick up new available payment methods
+export const revalidate = 300;
 
 export default function Page() {
-  return (
-    <main>
-      <CheckoutForm />
-    </main>
-  );
+    return (
+        <main>
+            <CheckoutForm />
+        </main>
+    );
 }

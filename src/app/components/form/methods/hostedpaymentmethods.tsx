74a6f50b4cfa3eba4cf41f07ec37a90049226185
Hannes Reinberger <40197225+hreinberger@users.noreply.github.com>
2025-01-21 15:12:21 +0100

    add product analytics with posthog

--- src/app/components/form/methods/hostedpaymentmethods.tsx
+++ src/app/components/form/methods/hostedpaymentmethods.tsx
@@ -5,6 +5,7 @@ import {
     Separator,
     Text,
     Callout,
+    Switch,
 } from '@radix-ui/themes';
 
 import React from 'react';
@@ -18,45 +19,81 @@ export default async function HostedPaymentMethods() {
     const methods = await mollieGetMethods();
 
     return (
-        <RadioGroup.Root
-            defaultValue={methods[0]?.id}
-            name="payment_method"
-        >
-            <Card m="1">
-                <Flex direction="column">
-                    {methods.map((method) => (
-                        <React.Fragment key={method.id}>
-                            <Flex
-                                align="center"
-                                justify="between"
-                                gap="4"
-                            >
-                                <Text as="label">
-                                    <Flex
-                                        gap="2"
-                                        align="center"
-                                    >
-                                        <RadioGroup.Item value={method.id} />
-                                        {method.description}
-                                    </Flex>
-                                </Text>
-                                <PaymentLogo method={method.id} />
-                            </Flex>
-                            <Separator
-                                my="3"
-                                size="4"
-                            />
-                        </React.Fragment>
-                    ))}
+        <>
+            <RadioGroup.Root
+                defaultValue={methods[0]?.id}
+                name="payment_method"
+            >
+                <Card m="1">
+                    <Flex direction="column">
+                        {methods.map((method) => (
+                            <React.Fragment key={method.id}>
+                                <Flex
+                                    align="center"
+                                    justify="between"
+                                    gap="4"
+                                >
+                                    <Text as="label">
+                                        <Flex
+                                            gap="2"
+                                            align="center"
+                                        >
+                                            <RadioGroup.Item
+                                                value={method.id}
+                                            />
+                                            {method.description}
+                                        </Flex>
+                                    </Text>
+                                    <PaymentLogo method={method.id} />
+                                </Flex>
+                                <Separator
+                                    my="3"
+                                    size="4"
+                                />
+                            </React.Fragment>
+                        ))}
 
-                    <Callout.Root>
-                        <Callout.Icon>
-                            <InfoCircledIcon />
-                        </Callout.Icon>
-                        <Callout.Text>Test Mode only!</Callout.Text>
-                    </Callout.Root>
-                </Flex>
-            </Card>
-        </RadioGroup.Root>
+                        <Callout.Root>
+                            <Callout.Icon>
+                                <InfoCircledIcon />
+                            </Callout.Icon>
+                            <Callout.Text>Test Mode only!</Callout.Text>
+                        </Callout.Root>
+                        <Separator
+                            my="3"
+                            size="4"
+                        />
+                        <Flex>
+                            <Text
+                                as="label"
+                                size="2"
+                            >
+                                <Flex
+                                    gap="2"
+                                    direction="column"
+                                >
+                                    <Flex gap="2">
+                                        <Switch
+                                            radius="full"
+                                            name="captureMode"
+                                            value={'manual'}
+                                        />
+                                        Authorize payment (Cards and Klarna
+                                        only)
+                                    </Flex>
+                                    <Text
+                                        size="1"
+                                        color="gray"
+                                    >
+                                        Authorized payments need to be captured
+                                        later
+                                    </Text>
+                                </Flex>
+                            </Text>
+                        </Flex>
+                    </Flex>
+                </Card>
+            </RadioGroup.Root>
+        </>
     );
 }

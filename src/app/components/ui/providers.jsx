600474079761b0fcae13a23471b8c0170db8f6f4
Hannes Reinberger <40197225+hreinberger@users.noreply.github.com>
2025-02-03 08:44:44 +0100

    remove posthog and add anonymous tracking via plausible

--- src/app/components/ui/providers.jsx
+++ src/app/components/ui/providers.jsx
@@ -1,20 +1,7 @@
 'use client';
 
 import { ThemeProvider } from 'next-themes';
-import posthog from 'posthog-js';
-import { PostHogProvider } from 'posthog-js/react';
-
-if (typeof window !== 'undefined') {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-        person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
-    });
-}
 
 export function Providers({ children }) {
-    return (
-        <PostHogProvider client={posthog}>
-            <ThemeProvider attribute="class">{children}</ThemeProvider>
-        </PostHogProvider>
-    );
+    return <ThemeProvider attribute="class">{children}</ThemeProvider>;
 }

80148d7f9f1472c3734890036cef140de7436305
Hannes Reinberger <40197225+hreinberger@users.noreply.github.com>
2025-02-04 14:55:12 +0100

    enlarge touch target

--- src/app/components/ui/paymentstable.tsx
+++ src/app/components/ui/paymentstable.tsx
@@ -69,6 +69,7 @@ export default async function PaymentsTable() {
                                     <IconButton
                                         variant="outline"
                                         aria-label="Details"
+                                        asChild
                                     >
                                         <Link href={'/payments/' + payment.id}>
                                             <MagnifyingGlassIcon />

67d6bfa528e7df5d9f81d6b7920fdc4d568a6a53
Hannes Reinberger <40197225+hreinberger@users.noreply.github.com>
2025-03-11 12:49:44 +0100

    fix build error (again)

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -4,7 +4,6 @@ import CheckoutForm from '../components/form/checkoutform';
 // since the form itself must be a client component, we import the server components here
 // and pass them as props to the form
 import Address from '../components/form/address';
-import ShoppingCart from '../components/form/shoppingcart';
 import HostedPaymentMethods from '../components/form/methods/hostedpaymentmethods';
 
 // invalidate page cache every 5 minutes to pick up new available payment methods
@@ -15,7 +14,6 @@ export default function Page() {
         <main>
             <CheckoutForm
                 address={<Address />}
-                cart={<ShoppingCart />}
                 hostedmethods={<HostedPaymentMethods />}
             />
         </main>

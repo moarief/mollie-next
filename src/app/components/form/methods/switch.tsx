f7ff2cdde9527b1d1599221ac3fe232fcc68526d
Hannes Reinberger <40197225+hreinberger@users.noreply.github.com>
2025-03-14 23:14:08 +0100

    suppress use of v1 mollie js in this branch

--- src/app/components/form/methods/switch.tsx
+++ src/app/components/form/methods/switch.tsx
@@ -10,42 +10,40 @@ import ComponentPaymentMethods from './componentpaymentmethods';
 import { CheckoutVariant } from '@/app/lib/types';
 
 export default function MethodSwitch({
-    hostedmethods,
-    variant,
-    onClick,
+  hostedmethods,
+  variant,
+  onClick,
 }: {
-    hostedmethods: React.ReactNode;
-    variant: CheckoutVariant;
-    onClick: (value: string) => void;
+  hostedmethods: React.ReactNode;
+  variant: CheckoutVariant;
+  onClick: (value: string) => void;
 }) {
-    // Use React State to switch between hosted and component payment methods
-    // If the State is switched to 'hosted', the HostedPaymentMethods server component is rendered
-    // If the State is switched to 'components', the ComponentPaymentMethods component is rendered
-    // Because HostedPaymentMethods is a server component, it is retrieved as a prop
-    return (
-        <>
-            <SegmentedControl.Root
-                size="1"
-                mx="1"
-                value={variant}
-                onValueChange={onClick}
-            >
-                <SegmentedControl.Item value="hosted">
-                    Hosted Checkout
-                </SegmentedControl.Item>
-                <SegmentedControl.Item value="components">
-                    Components
-                </SegmentedControl.Item>
-            </SegmentedControl.Root>
-            {variant === 'hosted' ? (
-                <Suspense fallback={MethodsSkeleton()}>
-                    {hostedmethods}
-                </Suspense>
-            ) : (
-                <Suspense fallback={MethodsSkeleton()}>
-                    <ComponentPaymentMethods />
-                </Suspense>
-            )}
-        </>
-    );
+  // Use React State to switch between hosted and component payment methods
+  // If the State is switched to 'hosted', the HostedPaymentMethods server component is rendered
+  // If the State is switched to 'components', the ComponentPaymentMethods component is rendered
+  // Because HostedPaymentMethods is a server component, it is retrieved as a prop
+  return (
+    <>
+      <SegmentedControl.Root
+        size="1"
+        mx="1"
+        value={variant}
+        onValueChange={onClick}
+      >
+        <SegmentedControl.Item value="hosted">
+          Hosted Checkout
+        </SegmentedControl.Item>
+        <SegmentedControl.Item value="components">
+          Components
+        </SegmentedControl.Item>
+      </SegmentedControl.Root>
+      {variant === 'hosted' ? (
+        <Suspense fallback={MethodsSkeleton()}>{hostedmethods}</Suspense>
+      ) : (
+        <Suspense fallback={MethodsSkeleton()}>
+          {/* <ComponentPaymentMethods /> */}
+        </Suspense>
+      )}
+    </>
+  );
 }

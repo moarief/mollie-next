b5e8a4d2a8c752b822b9c0d7d603759c2f9d2e01
Hannes Reinberger <40197225+hreinberger@users.noreply.github.com>
2025-03-17 10:19:18 +0100

    load the v2 script on the page and not in layout

--- src/app/express/page.tsx
+++ src/app/express/page.tsx
@@ -1,28 +1,32 @@
-import { RocketIcon } from '@radix-ui/react-icons';
-import { Callout, Flex, Heading } from '@radix-ui/themes';
+import { Flex, Heading } from '@radix-ui/themes';
 import { mollieCreateSession } from '../lib/mollie';
 import SessionWrapper from './components/SessionWrapper';
 import { ExpressSession } from '../lib/types';
+import Script from 'next/script';
 
 export default async function Page() {
-  const { sessionId, clientAccessToken } = await mollieCreateSession();
-  const session: ExpressSession = { id: sessionId, clientAccessToken };
+    const { sessionId, clientAccessToken } = await mollieCreateSession();
+    const session: ExpressSession = { id: sessionId, clientAccessToken };
 
-  return (
-    <main>
-      <Flex
-        direction="column"
-        m="6"
-      >
-        <Heading>Express Components</Heading>
-        <Flex
-          align="center"
-          justify="center"
-          mt="4"
-        >
-          <SessionWrapper session={session} />
-        </Flex>
-      </Flex>
-    </main>
-  );
+    return (
+        <main>
+            <Flex
+                direction="column"
+                m="6"
+            >
+                <Heading>Express Components</Heading>
+                <Flex
+                    align="center"
+                    justify="center"
+                    mt="4"
+                >
+                    <SessionWrapper session={session} />
+                </Flex>
+            </Flex>
+            <Script
+                src="https://js.mollie.com/v2/mollie.js"
+                strategy="beforeInteractive"
+            />
+        </main>
+    );
 }

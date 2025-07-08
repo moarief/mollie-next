da7e8467203aa980f5fe57af65473607d1dbe5fa
Hannes Reinberger <40197225+hreinberger@users.noreply.github.com>
2025-07-08 16:40:26 +0200

    add shopping cart mockup

--- src/app/express/page.tsx
+++ src/app/express/page.tsx
@@ -2,6 +2,7 @@ import { Flex, Heading } from '@radix-ui/themes';
 import { mollieCreateSession } from '../lib/mollie';
 import SessionWrapper from './components/SessionWrapper';
 import { ExpressSession } from '../lib/types';
+import ShoppingCart from '../components/form/shoppingcart';
 
 export default async function Page() {
     const { sessionId, clientAccessToken } = await mollieCreateSession();
@@ -19,7 +20,14 @@ export default async function Page() {
                     justify="center"
                     mt="4"
                 >
-                    <SessionWrapper session={session} />
+                    <Flex
+                        direction="column"
+                        align="stretch"
+                        gap="4"
+                    >
+                        <ShoppingCart />
+                        <SessionWrapper session={session} />
+                    </Flex>
                 </Flex>
             </Flex>
         </main>
